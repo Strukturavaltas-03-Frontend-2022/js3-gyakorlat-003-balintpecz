ed6a4045f226e7e16dca8d792614f2d66d0665d9
Balint Pecz <balint.pecz@evocrm.hu>
2022-09-01 06:52:19 +0200

    Done.

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,1 +1,31 @@
+'use strict'
 
+const cookieHandler = {
+
+    getAll() {
+        let obj = {};
+        const cookies = document.cookie.split("; ");
+        cookies.forEach((cookie) => {
+            const uniCookie = cookie.split("=");
+            obj[uniCookie[0]] = uniCookie[1];
+        });
+
+        return obj;
+    },
+
+    toSessionStorage() {
+        const obj = this.getAll();
+        Object.entries(obj).forEach(([key, value]) => {
+            sessionStorage.setItem(key, value);
+        });
+    },
+
+    flush() {
+        const obj = this.getAll();
+        Object.keys(obj).forEach((key) => {
+            document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+        });
+    },
+};
+
+export default cookieHandler;
