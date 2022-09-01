c4e9fd9f525646442f27c3bed2ba9edf15147907
Balint Pecz <balint.pecz@evocrm.hu>
2022-09-01 07:05:38 +0200

    mukodj

--- solutions/app.js
+++ solutions/app.js
@@ -1,4 +1,4 @@
-import setCookie from "./app1";
-import { cookieHandler } from "./app2";
+import setCookie from "./app1.js";
+import { cookieHandler } from "./app2.js";
 
 export { setCookie, cookieHandler };
